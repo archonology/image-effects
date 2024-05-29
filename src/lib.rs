9f9e6878d67470cbe8a01edda3a73767e900e683
Reed Meher <reed@meherdevs.com>
2024-05-29 13:16:46 -0500

    adding grayscale effect

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,18 @@
 use wasm_bindgen::prelude::*;
 use web_sys::console::log_1 as log;
 use base64::prelude::*;
+use image::load_from_memory;
 
 #[wasm_bindgen]
 pub fn grayscale(encoded_file: &str) {
     log(&"Grayscale called".into());
 
     let base64_to_vector = BASE64_STANDARD.decode(encoded_file).unwrap();
-    log(&"Image decoded".into())
+    log(&"Image decoded".into());
+
+    let mut img = load_from_memory(&base64_to_vector).unwrap();
+    log(&"Image loaded.".into());
+
+    img = img.grayscale();
+    log(&"grayscale effect applied".into());
 }
