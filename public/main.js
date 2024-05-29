c9c6c09c7b0aac961d7c44055d8dbb62ed30125f
Reed Meher <reed@meherdevs.com>
2024-05-29 11:49:32 -0500

    catching the image file

--- public/main.js
+++ public/main.js
@@ -1,1 +1,21 @@
-console.log('test');
+function init() {
+  const input = document.getElementById("upload");
+  // FileReader returns a string, easily managed by browsers and languages
+  const fileReader = new FileReader();
+
+  fileReader.onloadend = () => {
+    // make sure to remove the metadata with replace
+    const base64 = fileReader.result.replace(
+      /^data:image\/(png|jpeg|jpg);base64,/,
+      ""
+    );
+    console.log(input.files[0]);
+    console.log(base64);
+  };
+
+  input.addEventListener("change", () => {
+    fileReader.readAsDataURL(input.files[0]);
+  });
+}
+
+init();
